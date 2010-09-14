70e10ccccf646a93483eedfcdcbc8ad407675390
Hannes Bretschneider <habretschneider@gmail.com>
2010-09-15 06:20:27 +0800

    Corrected indentation of docstrings

--- flowvb/core/_lower_bound.py
+++ flowvb/core/_lower_bound.py
@@ -1,20 +1,20 @@
 class _LowerBound(object):
     """Class to compute and store the lower bound.
 
-Attributes used by this class:
+    Attributes used by this class:
 
-- prior_obj                   - The Prior object
+    - prior_obj                   - The Prior object
 
-- log_dirichlet_const0
-- log_wishart_const0
+    - log_dirichlet_const0
+    - log_wishart_const0
 
-- no_observations
-- no_dimensions
-- no_components
+    - no_observations
+    - no_dimensions
+    - no_components
 
-- log_dirichlet_normalisation0
-- log_dirichlet_normalisation
-"""
+    - log_dirichlet_normalisation0
+    - log_dirichlet_normalisation
+    """
 
     def __init__():
         """Initialize lower bound.
