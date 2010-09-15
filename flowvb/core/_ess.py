dc448b17563f14d5266389d3a4d772020eae6dbb
Hannes Bretschneider <habretschneider@gmail.com>
2010-09-14 22:11:03 -0700

    Further edits on docstrings

--- flowvb/core/_ess.py
+++ flowvb/core/_ess.py
@@ -2,15 +2,16 @@ class _ESS(object):
     """Class to compute and store the sufficient statistics.
 
     Attributes used by this class:
-    - mu                           - The centers of the clusters
-    - Sigma                        - Covariance matrices of the clusters
-    - pi                           - The mixing weights
-    - omega                        - Responsabilities scaled by the scale
+    - component_centers            - The centers of the clusters
+    - component_precisions         - Covariance matrices of the clusters
+    - mixing_weights               - The mixing weights
+    - responsabilities_scaled      - Responsabilities scaled by the scale
                                      variable
 
     - num_observations             - Number of observations (N in Arch2007)
     - num_dimensions               - Number of dimensions (D in Arch2007)
     - num_components               - Number of components (K in Arch2007)
+    
     """
     def __init__():
         """Initialize sufficient statistics.
--- flowvb/core/_posterior.py
+++ flowvb/core/_posterior.py
@@ -2,10 +2,21 @@ class _Posterior(object):
     """Class to compute and store the posterior parameters of the model.
 
     Attributes used by this class:
-    - kappa
-    - eta
-    - gamma
-    - 
+    - dirichlet_parameter_posterior    - Parameter to the Dirichlet distribution
+                                         (kappa_m in Arch2007)
+    - scale_prec_posterior             - Scale parameter for the precision matrix
+                                         (eta_m in Arch2007)
+    - wishart_dof_posterior            - Degrees of freedom of the Wishart distribution
+                                         (gamma_m in Arch2007)
+    - normal_mean_posterior            - Expectation of the Normal distribution
+                                         (m_m in Arch2007)
+    - wishart_precision_posterior      - Precision matrix of the Wishart distribution
+                                         (S_m in Arch2007)
+    - dof                              - Degrees of freedom of the components
+                                         (nu_m in Arch2007)
+
+    - no_observations                  - Number of observations
+    - no_components                    - No of components
     
     """
 
