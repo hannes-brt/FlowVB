9d76bc17dc9b0ec05632a1c46074c50b38c9b83b
Hannes Bretschneider <habretschneider@gmail.com>
2010-09-15 23:16:07 -0700

    Extended skeleton for _posterior.py

--- flowvb/core/_posterior.py
+++ flowvb/core/_posterior.py
@@ -37,3 +37,57 @@ class _Posterior(object):
 
         """
         pass
+
+    @staticmethod
+    def _update_dirichlet_parameter_posterior(no_observations,
+                                              mixweights,
+                                              dirichlet_parameter_prior):
+        """ Update `dirichlet_parameter_posterior` (Eq 27 in Arch2007) """
+        pass
+
+    @staticmethod
+    def _update_normal_wishart_scale_posterior(no_observations,
+                                               e_scaled_responsabilities,
+                                               normal_wishart_scale_prior):
+        """ Update `scale_prec_posterior` (Eq 28 in Arch2007) """
+        pass
+
+    @staticmethod
+    def _update_normal_wishart_mean_posterior(no_observations,
+                                              e_scaled_responsabilities,
+                                              normal_wishart_scale_prior,
+                                              normal_wishart_scale_posterior,
+                                              normal_wishart_mean_prior):
+        """ Update `normal_mean_posterior` (Eq 29 in Arch2007) """
+        pass
+
+    @staticmethod
+    def _update_normal_wishart_dof_posterior(no_observations,
+                                             mixweights,
+                                             normal_wishart_dof_prior):
+        """ Update `normal_wishart_dof_posterior` (Eq 30 in Arch2007) """
+        pass
+
+    @staticmethod
+    def _update_normal_wishart_scale_matrix_posterior(no_observations,
+                                                      e_component_mean,
+                                                      normal_wishart_mean_prior,
+                                                      e_scaled_responsabilities,
+                                                      e_component_covar,
+                                                      normal_wishart_scale_prior,
+                                                      normal_wishart_scale_posterior,
+                                                      normal_wishart_scale_matrix_prior):
+        """ Update `normal_wishart_scale_matrix_posterior` (Eq 31 in Arch2007) """
+        pass
+
+    @staticmethod
+    def _update_student_dof(no_observations,
+                            no_components,
+                            mixweights,
+                            e_responsabilities,
+                            e_scale_student,
+                            e_log_scale_student):
+        """ Update `student_dof` (Eq 36 in Arch2007) """
+        pass
+                                            
+                                            
