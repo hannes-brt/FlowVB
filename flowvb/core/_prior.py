88a9b0478ad389f37b54a18b25c29effd78bb8bd
Hannes Bretschneider <habretschneider@gmail.com>
2010-09-26 13:23:15 -0700

    Implemented `_Prior` using traits

--- flowvb/core/_prior.py
+++ flowvb/core/_prior.py
@@ -1,19 +1,37 @@
-class _Prior(object):
+from numpy import eye, mat
+from enthought.traits.api import HasTraits, Int, Float, Array
+
+class _Prior(HasTraits):
     """Class to compute and store the prior parameters of the model.
 
     """
 
-    def __init__(self):
+    num_obs = Int()
+    num_comp = Int()
+    num_features = Int()
+    
+    dirichlet = Float()
+    nws_mean = Float()
+    nws_scale = Float()
+    nws_dof = Float()
+    nws_scale_matrix = Array()
+
+
+    def __init__(self, data, num_comp, prior_dirichlet = 1e-3, nws_mean = 0,
+                 nws_scale = 1, nws_dof = 20, nws_scale_matrix = 1/200.):
         """Initialize prior paramters.
 
         """
-        pass
 
-    def update_parameters():
-        """Update prior parameters.
+        self.num_obs = data.shape[0]
+        self.num_features = data.shape[1]
+        self.num_comp = num_comp
 
-        """
-        pass
+        self.dirichlet = prior_dirichlet
+        self.nws_mean = nws_mean
+        self.nws_scale = nws_scale
+        self.nws_dof = nws_dof
+        self.nws_scale_matrix = nws_scale_matrix * mat(eye(self.num_features))
 
     def remove_clusters():
         """Remove clusters with insufficient support.
