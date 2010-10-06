ca642f9f48779dae13bea6f1745a2e49dac4e254
Hannes Bretschneider <habretschneider@gmail.com>
2010-10-05 22:34:25 -0700

    Added `repeat` function

--- flowvb/utils.py
+++ flowvb/utils.py
@@ -2,10 +2,23 @@
 """
 
 import numpy as np
+from scipy.maxentropy import logsumexp
+
+
+def repeat(x, func, *args, **kargs):
+    """Call the function `func` `x` times and accumulate the results in a list
+    """
+    return [func(*args, **kargs) for _ in range(x)]
+
 
 def normalize(vector):
     """Normalizes a vector to have sum one
     """
-    
+
     return np.array(vector) / sum(vector)
-       
+
+
+def normalize_logspace(a):
+    """Normalize the vector `a` in logspace """
+    L = logsumexp(a)
+    return a - L
