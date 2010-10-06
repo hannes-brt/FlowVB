bf1302b90244e9182a3b4cf3f9c9eb5a0c7ad73d
Hannes Bretschneider <habretschneider@gmail.com>
2010-10-05 22:08:20 -0700

    Added methods to compute lower bound (without body)

--- flowvb/core/_lower_bound.py
+++ flowvb/core/_lower_bound.py
@@ -26,3 +26,52 @@ class _LowerBound(HasTraits):
 
         """
         pass
+
+    def update_lower_bound(self):
+        """Update the lower bound """
+        pass
+
+    @staticmethod
+    def _log_dirichlet_const():
+        """Compute `log_dirichlet_const` """
+        pass
+
+    @staticmethod
+    def _log_wishart_const():
+        """Compute `log_wishart_const_init` """
+        pass
+
+    @staticmethod
+    def _expect_log_px():
+        """Compute `expect_log_px` (Eq 40 in Arch2007) """
+        pass
+
+    @staticmethod
+    def _expect_log_pu():
+        """Compute `expect_log_pu` (Eq 41 in Arch2007) """
+        pass
+
+    @staticmethod
+    def _expect_log_pz():
+        """Compute `expect_log_pz` (Eq 42 in Arch2007) """
+        pass
+
+    @staticmethod
+    def _expect_log_ptheta():
+        """Compute `expect_log_ptheta` (Eq 43 in Arch2007) """
+        pass
+
+    @staticmethod
+    def _expect_log_qu():
+        """Compute `expect_log_qu` (Eq 44 in Arch2007) """
+        pass
+
+    @staticmethod
+    def _expect_log_qz():
+        """Compute `expect_log_qz` (Eq 45 in Arch2007) """
+        pass
+
+    @staticmethod
+    def _expect_log_qtheta():
+        """"Compute `expect_log_qtheta` (Eq 46 in Arch2007) """
+        pass
