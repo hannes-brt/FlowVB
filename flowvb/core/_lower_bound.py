48e2a0ae5ba6d10f6e41f2706e091daa9d04067e
Hannes Bretschneider <habretschneider@gmail.com>
2010-11-02 23:32:09 -0700

    Tests for `_LowerBound`

--- flowvb/core/_lower_bound.py
+++ flowvb/core/_lower_bound.py
@@ -32,46 +32,72 @@ class _LowerBound(HasTraits):
         pass
 
     @staticmethod
-    def _log_dirichlet_const():
+    def _log_dirichlet_normalization_prior(num_comp, prior_dirichlet):
+        """Compute the normalization constant for the dirichlet distribution"""
+        pass
+
+    @staticmethod
+    def _log_dirichlet_normalization(num_obs, num_comp, prior_dirichlet):
+        """Compute the normalization constant for the dirichlet distribution"""
+        pass
+
+    @staticmethod
+    def _log_dirichlet_const(prior_dirichlet):
         """Compute `log_dirichlet_const` """
         pass
 
     @staticmethod
-    def _log_wishart_const():
+    def _log_wishart_const(num_features, posterior_nws_dof,
+                           posterior_nws_scale_matrix):
         """Compute `log_wishart_const_init` """
         pass
 
     @staticmethod
-    def _expect_log_px():
+    def _expect_log_px(num_obs, num_features, num_comp, latent_resp,
+                       latent_scale, latent_log_scale,
+                       posterior_nws_scale_matrix_inv, smm_mixweights,
+                       latent_scaled_resp, posterior_nws_dof,
+                       posterior_nws_scale, log_smm_mixweight):
         """Compute `expect_log_px` (Eq 40 in Arch2007) """
         pass
 
     @staticmethod
-    def _expect_log_pu():
+    def _expect_log_pu(num_obs, num_comp, smm_mixweights, smm_dof,
+                       latent_resp, latent_log_scale, latent_scaled_resp,
+                       expect_log_pu):
         """Compute `expect_log_pu` (Eq 41 in Arch2007) """
         pass
 
     @staticmethod
-    def _expect_log_pz():
+    def _expect_log_pz(num_comp, latent_resp, log_smm_mixweight):
         """Compute `expect_log_pz` (Eq 42 in Arch2007) """
         pass
 
     @staticmethod
-    def _expect_log_ptheta():
+    def _expect_log_ptheta(num_comp, num_features, prior_nws_mean,
+                           prior_dirichlet, prior_nws_dof, prior_nws_scale,
+                           prior_nws_scale_matrix, posterior_nws_mean,
+                           posterior_nws_dof, posterior_nws_scale,
+                           posterior_nws_scale_matrix_inv):
         """Compute `expect_log_ptheta` (Eq 43 in Arch2007) """
         pass
 
     @staticmethod
-    def _expect_log_qu():
+    def _expect_log_qu(num_obs, num_comp, gamma_param_alpha, gamma_param_beta,
+                       latent_resp, smm_mixweights):
         """Compute `expect_log_qu` (Eq 44 in Arch2007) """
         pass
 
     @staticmethod
-    def _expect_log_qz():
+    def _expect_log_qz(latent_resp):
         """Compute `expect_log_qz` (Eq 45 in Arch2007) """
         pass
 
     @staticmethod
-    def _expect_log_qtheta():
-        """"Compute `expect_log_qtheta` (Eq 46 in Arch2007) """
+    def _expect_log_qtheta(num_comp, num_features, log_wishart_const,
+                           log_dirichlet_normalization, posterior_dirichlet,
+                           posterior_nws_scale, posterior_nws_dof,
+                           posterior_nws_scale_matrix, log_smm_mixweight,
+                           log_det_precision):
+        """Compute `expect_log_qtheta` (Eq 46 in Arch2007) """
         pass
---
+++ tests/test_lower_bound.py
@@ -0,0 +1,104 @@
+import unittest
+from os.path import join
+from flowvb.core._lower_bound import _LowerBound
+from tests.test_old_faithful import makeTestFaithful
+
+TEST_ACCURACY = 3
+MAX_DIFF = pow(10, -TEST_ACCURACY)
+TEST_DATA_LOC = join('tests', 'data', 'old_faithful')
+
+TestLogDirichletNormalizationPrior = makeTestFaithful(
+    'log_dirichlet_normalization_prior.mat',
+    _LowerBound._log_dirichlet_normalization_prior,
+    ('num_comp', 'prior_dirichlet'),
+    'log_dirichlet_normalization_prior')
+
+TestLogDirichletNormalization = makeTestFaithful(
+    'log_dirichlet_normalization.mat',
+    _LowerBound._log_dirichlet_normalization,
+    ('num_obs', 'num_comp', 'prior_dirichlet'),
+    'log_dirichlet_normalization')
+
+TestLogDirichletConst = makeTestFaithful('log_dirichlet_const.mat',
+    _LowerBound._log_dirichlet_const,
+    ('prior_dirichlet',), 'log_dirichlet_const')
+
+TestLogWishartConst = makeTestFaithful('log_wishart_const.mat',
+    _LowerBound._log_wishart_const,
+    ('num_features', 'posterior_nws_dof',
+     'posterior_nws_scale_matrix'),
+    'log_wishart_const')
+
+TestExpectLogPx = makeTestFaithful('expect_log_px.mat',
+    _LowerBound._expect_log_px,
+    ('num_obs', 'num_features', 'num_comp', 'latent_resp',
+     'latent_scale', 'latent_log_scale',
+     'posterior_nws_scale_matrix_inv', 'smm_mixweights',
+     'latent_scaled_resp', 'posterior_nws_dof',
+     'posterior_nws_scale', 'log_smm_mixweight'),
+    'expect_log_px')
+
+TestExpectLogPu = makeTestFaithful('expect_log_pu.mat',
+    _LowerBound._expect_log_pu,
+    ('num_obs', 'num_comp', 'smm_mixweights', 'smm_dof',
+     'latent_resp', 'latent_log_scale', 'latent_scaled_resp',
+     'expect_log_pu'),
+    'expect_log_pu')
+
+TestExpectLogPz = makeTestFaithful('expect_log_pz.mat',
+    _LowerBound._expect_log_pz,
+    ('num_comp', 'latent_resp', 'expect_log_pz'),
+    'expect_log_pz')
+
+TestExpectLogPTheta = makeTestFaithful('expect_log_ptheta.mat',
+    _LowerBound._expect_log_ptheta,
+    ('num_comp', 'num_features', 'prior_nws_mean', 'prior_dirichlet',
+     'prior_nws_dof', 'prior_nws_scale', 'prior_nws_scale_matrix',
+     'posterior_nws_mean', 'posterior_nws_dof', 'posterior_nws_scale',
+     'posterior_nws_scale_matrix_inv'),
+    'expect_log_ptheta')
+
+TestExpectLogQu = makeTestFaithful('expect_log_qu.mat',
+    _LowerBound._expect_log_qu,
+    ('num_obs', 'num_comp', 'gamma_param_alpha', 'gamma_param_beta',
+     'latent_resp', 'smm_mixweights'),
+    'expect_log_qu')
+
+TestExpectLogQz = makeTestFaithful('expect_log_qz.mat',
+    _LowerBound._expect_log_qz,
+    ('latent_resp',), 'expect_log_qz')
+
+TestExpectLogQTheta = makeTestFaithful('expect_log_qtheta.mat',
+    _LowerBound._expect_log_qtheta,
+    ('num_comp', 'num_features', 'log_wishart_const',
+     'log_dirichlet_normalization', 'posterior_dirichlet',
+     'posterior_nws_scale', 'posterior_nws_dof',
+     'posterior_nws_scale_matrix', 'log_smm_mixweight',
+     'log_det_precision'),
+    'expect_log_qtheta')
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(
+        TestLogDirichletNormalizationPrior)
+    suite.addTest(
+        unittest.TestLoader().loadTestsFromTestCase(
+            TestLogDirichletNormalization))
+    suite.addTest(
+        unittest.TestLoader().loadTestsFromTestCase(TestLogDirichletConst))
+    suite.addTest(
+        unittest.TestLoader().loadTestsFromTestCase(TestLogWishartConst))
+    suite.addTest(
+        unittest.TestLoader().loadTestsFromTestCase(TestExpectLogPx))
+    suite.addTest(
+        unittest.TestLoader().loadTestsFromTestCase(TestExpectLogPu))
+    suite.addTest(
+        unittest.TestLoader().loadTestsFromTestCase(TestExpectLogPz))
+    suite.addTest(
+        unittest.TestLoader().loadTestsFromTestCase(TestExpectLogPTheta))
+    suite.addTest(
+        unittest.TestLoader().loadTestsFromTestCase(TestExpectLogQu))
+    suite.addTest(
+        unittest.TestLoader().loadTestsFromTestCase(TestExpectLogQz))
+    suite.addTest(
+        unittest.TestLoader().loadTestsFromTestCase(TestExpectLogQTheta))
+    unittest.TextTestRunner(verbosity=2).run(suite)
