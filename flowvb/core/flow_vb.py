Algorithm runs through for the first time and finds the correct cluster means in the Old Faithful data (but not the correct covariance matrices)

flowvb/core/flow_vb.py
@@ -6,9 +6,9 @@ from flowvb.core._latent_variables import _LatentVariables
 from flowvb.core._lower_bound import _LowerBound
 from flowvb.core._posterior import _Posterior
 from flowvb.core._prior import _Prior
-from flowvb.utils import element_weights, plot_ellipse
+from flowvb.utils import element_weights, plot_ellipse, classify_by_distance
 import matplotlib.pyplot as plt
-from pylab import gca
+import pudb
 
 EPS = np.finfo(np.float).eps
 
@@ -29,6 +29,9 @@ class FlowVB(HasTraits):
                  max_iter=200,
                  thresh=1e-5,
                  verbose=True,
+                 init_mean=None,
+                 init_covar=None,
+                 init_mixweights=None,
                  prior_dirichlet=1e-3,
                  dof_init=2,
                  remove_comp_thresh=1e-2,
@@ -43,18 +46,30 @@ class FlowVB(HasTraits):
             data = whiten(data)
 
         self.data = data
+        self.remove_comp_thresh = remove_comp_thresh
 
-        # Initialize with k-means
-        (centroids, labels) = kmeans2(data, num_comp_init)
-        smm_covar_init = [np.diag(np.ones(num_features))
-                          for _ in range(num_comp_init)]
-        smm_mixweights_init = element_weights(labels)
+        if init_mean is None:
+            # No starting solution was supplied
+            # Initialize with k-means
+            (init_mean, labels) = kmeans2(data, num_comp_init)
+            init_covar = self._get_covar(data, labels)
+            init_mixweights = element_weights(labels)
+
+        else:
+            # Starting solution was supplied
+            num_comp_init = init_mean.shape[0]
+            if init_mixweights is None:
+                labels = classify_by_distance(data, init_mean,
+                                              init_covar)
+                init_mixweights = element_weights(labels)
+            if init_covar is None:
+                init_covar = self._get_covar(data, labels)
 
         # Initialize data structures
         Prior = _Prior(data, num_comp_init, prior_dirichlet)
 
-        ESS = _ESS(data, num_comp_init, centroids, smm_covar_init,
-                   smm_mixweights_init)
+        ESS = _ESS(data, num_comp_init, init_mean, init_covar,
+                   init_mixweights)
 
         LatentVariables = _LatentVariables(data, ESS, num_comp_init)
 
@@ -67,41 +82,29 @@ class FlowVB(HasTraits):
         Posterior.update_parameters(Prior, ESS, LatentVariables)
 
         # Main loop
-        iter = 1
+        iteration = 1
         done = False
 
         while not done:
 
-            # Remove empty cluster
-            self.remove_empty_clusters(Prior, LatentVariables, ESS,
-                                       Posterior, LowerBound,
-                                       remove_comp_thresh)
-
-            # E-step
-            LatentVariables.update_parameters(Posterior)
-
-            # Compute ancilliary statistics
-            ESS.update_parameters(Prior, LatentVariables)
-
-            # M-step
-            Posterior.update_parameters(Prior, ESS, LatentVariables)
-
-            # Compute the lower bound
-            LowerBound.get_lower_bound(ESS, Prior, Posterior, LatentVariables)
+            # Update parameters
+            self._update_step(Prior, Posterior, ESS,
+                              LatentVariables, LowerBound)
 
             # Converged?
-            if iter == 1:
+            if iteration == 1:
                 converged = False
             else:
-                converged = self.convergence_test(LowerBound, thresh)
+                converged = self._convergence_test(LowerBound, thresh)
 
-            done = converged or (iter > max_iter)
+            done = converged or (iteration >= max_iter)
 
             if verbose:
                 print('iteration %d, lower bound: %f' %
-                      (iter, LowerBound.lower_bound[-1]))
+                      (iteration, LowerBound.lower_bound[-1]))
+                print Posterior.nws_mean
 
-            iter += 1
+            iteration += 1
 
         self.Posterior = Posterior
         self.Prior = Prior
@@ -109,21 +112,55 @@ class FlowVB(HasTraits):
         self.ESS = ESS
         self.LowerBound = LowerBound
 
-    def plot_clustering_ellipses(self, Posterior=None, dims=[0, 1]):
-        if Posterior is None:
-            Posterior = self.Posterior
+    def plot_clustering_ellipses(self, ESS=None, dims=[0, 1], scale=1):
+        if ESS is None:
+            ESS = self.ESS
 
         plt.plot(self.data[:, dims[0]], self.data[:, dims[1]], 'o', ls='none')
 
-        for k in range(Posterior.num_comp):
-            pos = Posterior.nws_mean[k, :]
-            cov = Posterior.nws_scale_matrix[k, :, :]
+        for k in range(ESS.num_comp):
+            pos = ESS.smm_mean[k, :]
+            cov = scale * ESS.smm_covar[k, :, :]
+            plt.plot(pos[0], pos[1], 'r+')
             plot_ellipse(pos, cov, edge='red')
 
         plt.show()
 
     @staticmethod
-    def convergence_test(LowerBound, thresh=1e-4):
+    def _remove_empty_clusters(Prior, LatentVariables, ESS, Posterior,
+                             LowerBound, remove_comp_thresh):
+
+        empty_cluster_indices = np.nonzero(
+            ESS.smm_mixweights < remove_comp_thresh)[0]
+        empty_cluster_indices = set(empty_cluster_indices)
+
+        if len(empty_cluster_indices) > 0:
+            Prior.remove_clusters(empty_cluster_indices)
+            LatentVariables.remove_clusters(empty_cluster_indices)
+            ESS.remove_clusters(empty_cluster_indices)
+            Posterior.remove_clusters(empty_cluster_indices)
+            LowerBound.remove_clusters(empty_cluster_indices)
+
+    def _update_step(self, Prior, Posterior, ESS, LatentVariables, LowerBound):
+        # E-step
+        LatentVariables.update_parameters(Posterior)
+
+        # Compute ancilliary statistics
+        ESS.update_parameters(Prior, LatentVariables)
+
+        # Remove empty cluster
+        self._remove_empty_clusters(Prior, LatentVariables, ESS,
+                                    Posterior, LowerBound,
+                                    self.remove_comp_thresh)
+
+        # M-step
+        Posterior.update_parameters(Prior, ESS, LatentVariables)
+
+        # Compute the lower bound
+        LowerBound.get_lower_bound(ESS, Prior, Posterior, LatentVariables)
+
+    @staticmethod
+    def _convergence_test(LowerBound, thresh=1e-4):
         converged = False
 
         fval = LowerBound.lower_bound[-1]
@@ -137,16 +174,21 @@ class FlowVB(HasTraits):
         return converged
 
     @staticmethod
-    def remove_empty_clusters(Prior, LatentVariables, ESS, Posterior,
-                             LowerBound, remove_comp_thresh):
+    def _get_covar(data, labels, *args, **kargs):
+        elements = range(max(labels) + 1)
 
-        empty_cluster_indices = np.nonzero(
-            ESS.smm_mixweights < remove_comp_thresh)[0]
-        empty_cluster_indices = set(empty_cluster_indices)
+        def covar(m):
+            # Make sure, a dxd-matrix is returned, even when there are
+            # only zero one observations
+            if len(m.shape) > 1:
+                d, n = m.shape
+            elif len(m.shape) == 1:
+                n = 1
+                d = m.shape[2]
+            if n > 1:
+                return np.cov(m, *args, **kargs)
+            else:
+                return np.zeros([d, d])
 
-        if len(empty_cluster_indices) > 0:
-            Prior.remove_clusters(empty_cluster_indices)
-            LatentVariables.remove_clusters(empty_cluster_indices)
-            ESS.remove_clusters(empty_cluster_indices)
-            Posterior.remove_clusters(empty_cluster_indices)
-            LowerBound.remove_clusters(empty_cluster_indices)
+        return np.array([covar(data[labels == l, :].T)
+                         for l in elements])

flowvb/utils.py
@@ -7,7 +7,7 @@ from numpy.linalg import cholesky
 from scipy.maxentropy import logsumexp
 from scipy.special import gammaln
 import math
-from math import pi
+from math import pi, sqrt
 from matplotlib.patches import Ellipse
 from pylab import gca
 
@@ -127,3 +127,20 @@ def plot_ellipse(pos, P, edge='black', face='none'):
     ax = gca()
     ax.add_patch(ellipsePlot)
     return ellipsePlot
+
+
+def classify_by_distance(data, mean, covar):
+    K = mean.shape[0]
+
+    covar_inv = np.array([np.linalg.inv(covar[k, :, :]) for k in range(K)])
+
+    def mahal_dist(x, k):
+        return sqrt(np.dot(np.dot((x - mean[k, :]).T, covar_inv[k, :, :]),
+                    (x - mean[k, :])))
+
+    def classify_point(x):
+        dist = np.array([mahal_dist(x, k) for k in range(K)])
+        return np.nonzero(dist == min(dist))[0]
+
+    labels = np.apply_along_axis(classify_point, 1, data)
+    return labels