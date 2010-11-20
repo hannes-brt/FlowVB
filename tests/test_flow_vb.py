77652b09e8dc17431f0a3c2e9bb27021ce61a6d5
Hannes Bretschneider <habretschneider@gmail.com>
2010-11-20 15:07:05 -0800

    Added a test with synthetic data

--- tests/test_flow_vb.py
+++ tests/test_flow_vb.py
@@ -3,11 +3,15 @@ from os.path import join
 from flowvb import FlowVB
 from scipy.io import loadmat
 from flowvb.utils import arrays_almost_equal
+import numpy as np
+from numpy.random import multivariate_normal as rmvnorm
 
 TEST_DATA_LOC = join('tests', 'data', 'old_faithful')
 
 
 class TestFaithul(unittest.TestCase):
+    """Test with the Old Faithful data """
+
     def setUp(self):
         self.data = loadmat(join(TEST_DATA_LOC, 'faithful.mat'))['data']
         self.init = loadmat(join(TEST_DATA_LOC, 'faithful_init.mat'),
@@ -29,6 +33,37 @@ class TestFaithul(unittest.TestCase):
         self.assertTrue(approx_equal)
 
 
+class TestRandom(unittest.TestCase):
+    """Test with some synthetic data """
+
+    def setUp(self):
+        np.random.seed(0)
+
+        self.mean = np.array([[0., -2.], [0., 0.], [0., 2.]])
+        self.cov = np.array([[2., 0.], [0., .2]])
+
+        n_obs = 2000
+
+        data = np.vstack([np.array(rmvnorm(self.mean[k, :], self.cov, n_obs))
+                          for k in range(self.mean.shape[0])])
+
+        self.model = FlowVB(data, 6, thresh=1e-6, init_method='random')
+
+    def runTest(self):
+
+        # Test for the correct number of components
+        self.assertEqual(self.model.ESS.num_comp, 3)
+
+        # Test the means
+        sorted_mean = np.sort(self.model.ESS.smm_mean, 0)
+        approx_equal = arrays_almost_equal(sorted_mean,
+                                           self.mean,
+                                           accuracy=1e-1)
+        self.assertTrue(approx_equal)
+
+
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestFaithul)
+    suite.addTest(
+        unittest.TestLoader().loadTestsFromTestCase(TestRandom))
     unittest.TextTestRunner(verbosity=2).run(suite)
