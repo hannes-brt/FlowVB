001767b709d423eece748deef564489f0d45ed93
Hannes Bretschneider <habretschneider@gmail.com>
2010-12-10 08:49:33 -0800

    Revert "Merged `_monitor_plot.py` from graphics-branch"
    This reverts commit 4fa80d6ed836b7e2d9740bfc5ec0a4ecbe8e3fee.

--- flowvb/core/_monitor_plot.py
+++ flowvb/core/_monitor_plot.py
@@ -7,12 +7,12 @@ from matplotlib.patches import Ellipse
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_wxagg import \
      FigureCanvasWxAgg as FigureCanvas
-from mpl_toolkits.axes_grid.anchored_artists import AnchoredText
+
+TIMER_ID = wx.NewId()
 
 
 class _MonitorPlot(wx.Frame):
-    """Class to provide live updated plotting to monitor the
-    convergence of FlowVB
+    """Class to provide live updated plotting to monitor the convergence of FlowVB
     """
 
     def __init__(self, data, scale=1):
@@ -37,13 +37,8 @@ class _MonitorPlot(wx.Frame):
         self.ax.set_ylim(y_lims)
         self.ax.set_autoscale_on(False)
 
-        # Draw the data
-        self.l_data = self.ax.plot(self.data[:, 0], self.data[:, 1],
-                                   color='blue', linestyle='', marker='o')
-
         # Draw to screen
         self.canvas.draw()
-
         # Save background
         self.bg = self.canvas.copy_from_bbox(self.ax.bbox)
 
@@ -55,9 +50,12 @@ class _MonitorPlot(wx.Frame):
         self.canvas.restore_region(self.bg)
 
         # Remove previous elements from plot
-        self.ax.lines = [self.ax.lines[0]]  # Delete everything except
-                                            # scatterplot
+        self.ax.lines = []
         self.ax.patches = []
+
+        # Draw the data
+        self.l_data = self.ax.plot(self.data[:, 0], self.data[:, 1],
+                                   color='blue', linestyle='', marker='o')
 
         K = pos.shape[0]
         for k in range(K):
@@ -71,28 +69,13 @@ class _MonitorPlot(wx.Frame):
             ellipsePlot = Ellipse(xy=pos[k, :], width=2.0 * math.sqrt(s[0]),
                                   height=2.0 * math.sqrt(s[1]),
                                   angle=orient, facecolor='none',
-                                  edgecolor='red', zorder=100)
+                                  edgecolor='red')
 
             self.ax.add_patch(ellipsePlot)
 
         # Draw to screen
         self.canvas.draw()
         self.canvas.blit(self.ax.bbox)
-
-    def end_of_iteration(self):
-        self.button = wx.Button(self.canvas, label="Close Window",
-                                pos=(680, 10))
-        self.Bind(wx.EVT_BUTTON, self.OnButtonClick, self.button)
-        at = AnchoredText("Iteration finished.",
-                          prop=dict(size=8), frameon=True,
-                          loc=2)
-        at.patch.set_boxstyle("round,pad=0.,rounding_size=0.2")
-        self.ax.add_artist(at)
-        self.canvas.draw()
-
-    def OnButtonClick(self, event):
-        self.Close(True)
-
 
 if __name__ == "__main__":
     data = loadmat("tests/data/old_faithful/faithful.mat")['data']
