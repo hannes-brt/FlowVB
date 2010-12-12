18808ecb5ef53c41b214a5ce11ae4b3e9bc9e80b
Hannes Bretschneider <habretschneider@gmail.com>
2010-12-11 22:01:24 -0800

    Demo with Old Faithful

---
+++ demos/demo_old_faithful.py
@@ -0,0 +1,17 @@
+from os.path import join
+from flowvb import FlowVB
+from scipy.io import loadmat
+import numpy as np
+
+
+TEST_DATA_LOC = join('tests', 'data', 'old_faithful')
+
+np.random.seed(0)
+
+data = loadmat(join(TEST_DATA_LOC, 'faithful.mat'))['data']
+model = FlowVB(data,
+               num_comp_init=6,
+               thresh=1e-5,
+               max_iter=200,
+               verbose=False,
+               plot_monitor=True)
