4d7750d09da0b2e53437242514e6c0def4a50b15
Hannes Bretschneider <habretschneider@gmail.com>
2010-12-11 18:50:09 -0800

    Implemented `normalize_logspace` in C

--- flowvb/setup.py
+++ flowvb/setup.py
@@ -3,8 +3,10 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import numpy as np
 
-ext_modules = [Extension("cython_utils", ["cython_utils.pyx"],
+ext_modules = [Extension("normalize",
+                         sources=["normalize.pyx", "c-utils.c"],
                          include_dirs=[np.get_include()])]
+
 
 setup(
   name='FlowVB',
