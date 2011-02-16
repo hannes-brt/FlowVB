1d29fe97194e7f5afebc38be99cf532242323b03
Andrew Roth <andrewjlroth@gmail.com>
2011-02-15 19:07:34 -0800

    Added constants file to tests for storing common paths.

---
+++ tests/constants.py
@@ -0,0 +1,11 @@
+'''
+Created on 2011-02-15
+
+@author: Andrew Roth
+'''
+import os.path
+
+cwd = os.getcwd()
+data_folder = os.path.join( cwd, 'data' ) 
+
+TEST_DATA_LOC = os.path.join( data_folder, 'old_faithful' )
