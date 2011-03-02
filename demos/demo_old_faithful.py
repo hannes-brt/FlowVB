7190a20112bc802d4a5252f0ac361f11f1354cbc
Hannes Bretschneider <habretschneider@gmail.com>
2011-03-02 22:04:52 +0100

    adapted demos after refactoring

--- demos/demo_old_faithful.py
+++ demos/demo_old_faithful.py
@@ -1,7 +1,9 @@
 from os.path import join
 from flowvb import FlowVBAnalysis
+from flowvb.initialize import D2Initialiser
 from scipy.io import loadmat
 import numpy as np
+from argparse import Namespace
 
 
 TEST_DATA_LOC = join('../', 'tests', 'data', 'old_faithful')
@@ -9,9 +11,22 @@ TEST_DATA_LOC = join('../', 'tests', 'data', 'old_faithful')
 np.random.seed(0)
 
 data = loadmat(join(TEST_DATA_LOC, 'faithful.mat'))['data']
-model = FlowVBAnalysis(data,
-               num_comp_init=6,
-               thresh=1e-5,
-               max_iter=200,
-               verbose=False,
-               plot_monitor=True)
+
+args = Namespace()
+
+args.num_comp_init = 6
+args.thresh = 1e-5
+args.max_iter = 200
+args.verbose = False
+        
+args.prior_dirichlet = 1e-2
+args.dof_init = 2
+args.remove_comp_thresh = 1e-6
+        
+args.use_exact = False
+args.whiten_data = False
+args.plot_monitor = True
+
+args.init_params = D2Initialiser().initialise_parameters(data, args.num_comp_init)
+
+model = FlowVBAnalysis(data, args)
--- demos/demo_synth_1.py
+++ demos/demo_synth_1.py
@@ -1,6 +1,8 @@
 import numpy as np
 from numpy.random import multivariate_normal as rmvnorm
 from flowvb import FlowVBAnalysis
+from flowvb.initialize import D2Initialiser
+from argparse import Namespace
 
 """ Demo using synthetic data with three wide but short clusters """
 
@@ -14,6 +16,21 @@ n_obs = 2000
 data = np.vstack([np.array(rmvnorm(mean[k, :], cov, n_obs))
                   for k in range(mean.shape[0])])
 
-model = FlowVBAnalysis(data, 6, thresh=1e-5, init_method='d2-weighting',
-               plot_monitor=True, max_iter=10000,
-               remove_comp_thresh=1e-4)
+args = Namespace()
+
+args.num_comp_init = 6
+args.thresh = 1e-5
+args.max_iter = 10000
+args.verbose = False
+        
+args.prior_dirichlet = 1e-2
+args.dof_init = 2
+args.remove_comp_thresh = 1e-4
+        
+args.use_exact = False
+args.whiten_data = False
+args.plot_monitor = True
+
+args.init_params = D2Initialiser().initialise_parameters(data, args.num_comp_init)
+
+model = FlowVBAnalysis(data, args)
