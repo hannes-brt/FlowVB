2c4ebbde7401723a37c7536ff1648e0cb5b79fcf
Andrew Roth <andrewjlroth@gmail.com>
2011-05-13 16:19:21 -0700

    Update synthetic demos to use new options interface.

--- demos/demo_synth_2.py
+++ demos/demo_synth_2.py
@@ -1,12 +1,15 @@
 import numpy as np
 from numpy.random import multivariate_normal as rmvnorm
+
 from flowvb import FlowVBAnalysis
+from flowvb.core.flow_vb import Options
 from flowvb.initialize import D2Initialiser
-from argparse import Namespace
 
-""" Demo using synthetic data with a large and a small cluster """
+'''
+Demo using synthetic data with a large and a small cluster
+'''
 
-#np.random.seed(0)
+np.random.seed(0)
 
 mean = np.array([[0., -2.], [3., 3.]])
 cov = np.array([[[2., 0.], [0., .2]], [[.2, 0], [0, .2]]])
@@ -16,21 +19,10 @@ n_obs = [2000, 100]
 data = np.vstack([np.array(rmvnorm(mean[k, :], cov[k, :, :], n_obs[k]))
                   for k in range(mean.shape[0])])
 
-args = Namespace()
+num_comp_init = 6
+init_params = D2Initialiser().initialise_parameters(data, num_comp_init)
+options = Options(init_params)
 
-args.num_comp_init = 2
-args.thresh = 1e-5
-args.max_iter = 10000
-args.verbose = False
-        
-args.prior_dirichlet = 1e-2
-args.dof_init = 2
-args.remove_comp_thresh = 1e-6
-        
-args.use_exact = False
-args.whiten_data = False
-args.plot_monitor = True
+options.plot_monitor = True
 
-args.init_params = D2Initialiser().initialise_parameters(data, args.num_comp_init)
-
-model = FlowVBAnalysis(data, args)
+model = FlowVBAnalysis(data, options)
